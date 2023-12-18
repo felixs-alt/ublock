ba1ad08edd7ce266445a6ef87251f36f6afd3020
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-12-18 10:43:23 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-for (let i = 1; i < 50001; i++) {
-    setTimeout(function(){
-        console.log("Robux:"+i)
-    }, 1);
-}
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 220) {
+      for (let i = 1; i < 50001; i++) {
+            setTimeout(function(){
+                console.log("Robux:"+i)
+            }, 1);
+        }
+  }
+})
