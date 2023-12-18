de1460298318b1b1ad38130b845a29e2bd705401
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-12-18 10:47:36 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 220) {
-    alert("piss")
+    let _button=4;
+    let _ID;
+    document.body.addEventListener('mousedown', (e)=>{if(e.button===_button){_ID=_ID||setInterval(()=>e.target.click(),1)}});
+    document.body.addEventListener('mouseup', (e)=>{if(e.button===_button){clearInterval(_ID);_ID=0}});
   }
 })
