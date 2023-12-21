9cde237b3fdddd5188da8bcd26e1d3268c7dbc86
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-12-21 12:53:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,12 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 220) {
-    if (!window.location.host != "uk-www.securly.com"){
-      alert("not blocked")
-    }
+    if (!window.location.host != "uk-www.securly.com"){alert("not blocked")}
     function encode(str) {
     if (!str) return str;
     return "https://ultvt.us.to/uv/service/"+encodeURIComponent(
-        str
-        .toString()
-        .split('')
-        .map((char, ind) =>
-            ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-        )
-        .join('')
-        );
-  }
+        str.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));}
   const queryString = window.location.search;
-
   const urlParams = new URLSearchParams(queryString);
   window.location = encode("https://"+atob(decodeURI(urlParams.get('url'))))
   }
